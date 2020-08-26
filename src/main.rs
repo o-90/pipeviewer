126ad438d47743773e8b0f5ea455e05e4291a23c
gobrewers14 <john.r.martinez14@gmail.com>
2020-08-26 15:19:18 +0200

    completed seciton 2.3

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use std::env;
 use std::io::{self, Read, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
     let mut total_bytes = 0;
     loop {
         let mut buffer = [0; CHUNK_SIZE];
         let num_read = match io::stdin().read(&mut buffer) {
-            Ok(0)  => break,
-            Ok(x)  => x,
+            Ok(0) => break,
+            Ok(x) => x,
             Err(_) => break,
         };
         total_bytes += num_read;
         io::stdout().write_all(&buffer[..num_read]).unwrap();
     }
-    eprintln!("{}", total_bytes);
+    if !silent {
+        eprintln!("{}", total_bytes);
+    }
 }
