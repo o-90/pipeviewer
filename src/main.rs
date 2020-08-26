f50d8e199a18baa82b0e68de2022a859637c123b
gobrewers14 <john.r.martinez14@gmail.com>
2020-08-26 20:15:45 +0200

    completed seciton 3.1

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 /**
- * Hands-On Systems Programming in Rust 
+ * Hands-On Systems Programming in Rust
  * https://learning.oreilly.com/videos/hands-on-systems-programming/
  */
-
 use pipeviewer::{args::Args, read, stats, write};
 use std::io::Result;
 
 fn main() -> Result<()> {
-  let args = Args::parse();
-  let mut total_bytes = 0;
-  loop {
-    let buffer = match read::read(&args.infile) {
-      Ok(x) if x.is_empty() => break,
-      Ok(x) => x,
-      Err(_) => break,
-    };
-    stats::stats(args.silent, buffer.len(), &mut total_bytes, false);
-    if !write::write(&args.outfile, &buffer)? {
-      break;
+    let args = Args::parse();
+    let mut total_bytes = 0;
+    loop {
+        let buffer = match read::read(&args.infile) {
+            Ok(x) if x.is_empty() => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        stats::stats(args.silent, buffer.len(), &mut total_bytes, false);
+        if !write::write(&args.outfile, &buffer)? {
+            break;
+        }
     }
-  }
-  stats::stats(args.silent, 0, &mut total_bytes, true);
-  Ok(())
+    stats::stats(args.silent, 0, &mut total_bytes, true);
+    Ok(())
 }
